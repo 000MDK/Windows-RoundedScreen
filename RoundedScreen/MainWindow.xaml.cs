943603baaf4c68d178b97730f9b4c41f29d53b06
MDK <86747696+000MDK@users.noreply.github.com>
2025-03-08 18:53:47 +0100

    Update MainWindow.xaml.cs -- reload on displaysettingschange

--- RoundedScreen/MainWindow.xaml.cs
+++ RoundedScreen/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace RoundedScreen
@@ -24,6 +23,7 @@ namespace RoundedScreen
         {
             InitializeComponent();
             this.SetStartup();
+            SystemEvents.DisplaySettingsChanged += OnDisplaySettingsChanged;
         }
 
         private void SetStartup()
@@ -41,23 +41,42 @@ namespace RoundedScreen
             SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW);
         }
 
-        private void WndRoundedScreen_Closing(object sender, System.ComponentModel.CancelEventArgs e) { e.Cancel = true; }
+        private void WndRoundedScreen_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
 
         private void WndRoundedScreen_LostFocus(object sender, RoutedEventArgs e)
         {
             this.Activate();
-            this.Topmost = true; 
-            this.Topmost = false; 
-            this.Focus();      
+            this.Topmost = true;
+            this.Topmost = false;
+            this.Focus();
         }
 
         private void WndRoundedScreen_Loaded(object sender, RoutedEventArgs e)
         {
+            AdjustWindowSizeAndPosition();
+        }
+
+        private void AdjustWindowSizeAndPosition()
+        {
             Point location = this.PointToScreen(new Point(0, 0));
             this.WindowStartupLocation = WindowStartupLocation.Manual;
+            this.Width = SystemParameters.PrimaryScreenWidth;
+            this.Height = SystemParameters.PrimaryScreenHeight;
+        }
 
-            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
-            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+        private void OnDisplaySettingsChanged(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+        }
+
+
+        ~MainWindow()
+        {
+            SystemEvents.DisplaySettingsChanged -= OnDisplaySettingsChanged;
         }
     }
 }
